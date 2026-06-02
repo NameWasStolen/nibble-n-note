2da003859a70fad2e942e17f8a151d9d072e1427
James Chea <jche0346@student.monash.edu>
2026-06-03 04:43:20 +1000

    Edit RestaurantReviewEntry to align with data model

--- backend/models/RestaurantReviewEntry.js
+++ backend/models/RestaurantReviewEntry.js
@@ -1,7 +1,34 @@
 const mongoose = require('mongoose');
+const ratingSchema = require('./subschemas/ratingSchema');
 
-const restaurantReviewEntrySchema = new mongoose.Schema({
-    
-}, {timestamps: true }); // createdAt, updatedAt fields
+const restaurantReviewEntrySchema = new mongoose.Schema(
+    {
+        restaurantReviewId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'RestaurantReview',
+            required: true
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        userRating: {
+            type: ratingSchema,
+            required: true
+        },
+        images: {
+            type: [String],
+            default: []
+        }
+    },
+    { timestamps: true }
+);
+
+// One user can only have one entry per restaurant review
+restaurantReviewEntrySchema.index(
+    { restaurantReviewId: 1, userId: 1 },
+    { unique: true }
+);
 
 module.exports = mongoose.model('RestaurantReviewEntry', restaurantReviewEntrySchema);
