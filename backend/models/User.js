6edfebfbb8e4976663047e92b62e776c1c14354d
James Chea <jche0346@student.monash.edu>
2026-06-03 03:58:33 +1000

    Edit User model to align with data model

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,17 +8,18 @@ const userSchema = new mongoose.Schema({
     },
     name: { 
         type: String, 
-        required: true 
+        required: true,
+        trim: true, 
+        maxlength: 100 // Max reasonable name length
     },
     email: { 
         type: String, 
         required: true, 
-        unique: true 
-    },
-    createdAt: { 
-        type: Date, 
-        default: Date.now 
+        unique: true, 
+        lowercase: true, 
+        trim: true, 
+        maxlength: 254 // Max valid email length
     }
-});
+}, {timestamps: true }); // createdAt, updatedAt fields
 
 module.exports = mongoose.model('User', userSchema);
