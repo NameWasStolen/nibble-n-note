b2c578d9389ff362bd59aa2f0bccc26929bff181
James Chea <jche0346@student.monash.edu>
2026-06-06 22:07:32 +1000

    Add missing import authMiddleware to userRouter

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/userController.js');
+const authMiddleware = require('../middleware/authMiddleware.js');
 
 // Create router
 const router = express.Router();
