7de40a8bf750355840cdd592de7b6cd699e07b30
James Chea <jche0346@student.monash.edu>
2026-06-14 23:42:52 +1000

    Create restaurantReviewEntryController file

---
+++ backend/controllers/restaurantReviewEntryController.js
@@ -0,0 +1,9 @@
+// Add Mongoose models + other imports here
+const RestaurantReview = require('../models/RestaurantReview');
+const RestaurantReviewEntry = require('../models/RestaurantReviewEntry');
+const restaurantReviewService = require('../services/restaurantReviewService');
+const mongoose = require('mongoose');
+
+module.exports = {
+    createRestaurantReviewEntry: async (req, res) => {}
+}
