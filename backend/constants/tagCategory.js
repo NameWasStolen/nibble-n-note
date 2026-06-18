a1a39217a5c38f2b1b85a50bbf6e2893532a5b94
James Chea <jche0346@student.monash.edu>
2026-06-19 00:16:35 +1000

    Update all tag categories to use constants

---
+++ backend/constants/tagCategory.js
@@ -0,0 +1,11 @@
+const TAG_CATEGORIES = Object.freeze({
+    RESTAURANT: "Restaurant",
+    DISH: "Dish"
+})
+
+const TAG_CATEGORY_SOURCE = Object.freeze(Object.values(CONSENSUS_SOURCES))
+
+module.exports = {
+    TAG_CATEGORIES,
+    TAG_CATEGORY_SOURCE
+}
